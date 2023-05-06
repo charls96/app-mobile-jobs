efa38bc1afb9eb5ec9d77d0576d8e2c14467832f
Carlos Lopez Perez <clo@efficy.com>
2023-05-06 18:05:55 +0200

    popular jobs component showing the popular jobs cards

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from 'react';
+import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native';
+import { useRouter } from 'expo-router';
 
 import styles from './popularjobs.style'
+import {COLORS, SIZES } from '../../../constants';
+import PopularJobCard from '../../common/cards/popular/PopularJobCard';
 
 const Popularjobs = () => {
+  const router = useRouter();
+  const isLoading = false;
+  const error = false;
+
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size="large" colors={COLORS.primary} />
+        ) : error ? (
+          <Text style={styles.headerTitle}>Popular jobs</Text>
+        ) : (
+          <FlatList 
+            data={[1, 2, 3, 4]}
+            renderItem={({ item }) => (
+              <PopularJobCard 
+                item={item}
+              />
+            )}
+            keyExtractor={item => item?.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
+          />
+        )}
+      </View>
     </View>
   )
 }
