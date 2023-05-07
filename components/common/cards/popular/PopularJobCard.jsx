8a21e7dba97b3896346a98bcc71ddddedec3aae2
Carlos Lopez Perez <clo@efficy.com>
2023-05-07 16:00:09 +0200

    feat(images): check url image exists and if not give a default image

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,6 +1,8 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from 'react-native';
 
-import styles from './popularjobcard.style'
+import styles from './popularjobcard.style';
+
+import { checkImageURL } from '../../../../utils';
 
 const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
   return (
@@ -10,7 +12,10 @@ const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
     >
       <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
         <Image 
-          source={{ uri: item.employer_logo }}
+          source={{ uri: checkImageURL(item.employer_logo)
+          ? item.employer_logo
+          : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg"
+        }}
           resizeMode="contain"
           style={styles.logoImage}
         />
